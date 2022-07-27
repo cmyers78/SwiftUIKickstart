19220a76f98435d221dc28c680a91d46746ba0d7
Chris Myers <cmyers78@gmail.com>
2022-07-27 16:05:37 -0400

    initial commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Chris Myers on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
